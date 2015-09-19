ff70f05e172751c1163b7eeaba806a6f7ab48d1f
Prad <pra2nepal@yahoo.co.uk>
2015-09-19 13:19:19 +1000

    Create Trial
    Create Trial

--- Module 1 & 2/Professional Experience/Professional Experience/Controllers/AdministratorController.cs
+++ Module 1 & 2/Professional Experience/Professional Experience/Controllers/AdministratorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Professional_Experience.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +7,43 @@ using System.Web.Mvc;
 
 namespace Professional_Experience.Controllers
 {
-    public class AdministratorController : Controller
+    public class AdministratorController : UIController
     {
         // GET: Administrator
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult CreateTrial()
+        {
+            var m = new CreateTrialViewModel();
+            m.StartDate = DateTime.Now;
+            m.EndDate = DateTime.Now;
+            return View(m);
+        }
+
+        [HttpPost]
+        public ActionResult CreateTrial(CreateTrialViewModel m)
+        {
+            if (ModelState.IsValid)
+            {
+                var trial = new PX_Model.Trial();
+
+                trial.Name = m.Name;
+                trial.Description = m.Description;
+                trial.Start_Date = m.StartDate;
+                trial.End_Date = m.EndDate;
+                trial.Hypothesis = m.Hypothesis;
+                trial.Outcome = m.Outcome;
+                trial.Objectives = m.Objective;
+
+                _db.Trials.Add(trial);
+                _db.SaveChanges();
+
+                return View("Index");
+            }
+            return View(m);
         }
     }
 }
---
+++ Module 1 & 2/Professional Experience/Professional Experience/Models/CreateTrialViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Professional_Experience.Models
+{
+    public class CreateTrialViewModel
+    {
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Required]
+        [Display(Name = "Objective")]
+        public string Objective { get; set; }
+
+        [Required]
+        [Display(Name = "Hypothesis")]
+        public string Hypothesis { get; set; }
+
+        [Required]
+        [Display(Name = "Outcome")]
+        public string Outcome { get; set; }
+    }
+}
