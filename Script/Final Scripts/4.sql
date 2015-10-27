ab18bac461342c0f56145dc083184f4fe14ea8ba
Prad <pra2nepal@yahoo.co.uk>
2015-10-27 16:56:12 +1100

    Final Scripts
    Final Scripts

---
+++ Script/Final Scripts/4.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Participant_Intervention_Area_API_Key (
+	Id INT IDENTITY(1,1) NOT NULL,
+	Participant_Id INT,
+	Intervention_Area_Test_Id INT,
+	API_Key VARCHAR(50),
+	PRIMARY KEY (Id),
+	FOREIGN KEY (Participant_Id) REFERENCES Participant(Id),
+	FOREIGN KEY (Intervention_Area_Test_Id) REFERENCES Intervention_Area_Test(Id));
+
+
